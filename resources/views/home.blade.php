9db32f762ce64ed0ba2a4ee41d4df3a4d350f93b
newtella <fl.mlopezgt@gmail.com>
2020-11-27 09:11:41 -0600

    revert commit Proyecto DiacoApp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,10 @@
 
 <div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1423655156442-ccc11daa4e99?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450');">
        
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-12">
-                <div class="card card-signup">
-
-                
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
@@ -25,18 +23,12 @@
                </div>
                 </div>
               
-                    <div class="wrapper">
-                    <div class="row">
-                    <!-- <canvas class="my-4 chartjs-render-monitor" id="bar-chart" width="600" height="250" style="display: block; width: 586px; height: 247px;"></canvas> -->
-                      
-
-                    <canvas class="my-4 chartjs-render-monitor" id="bar-chart" style="height: 10rem"></canvas>
-
-                
-                    </main>
-                
-</div>
+                <div class="card">
+                <canvas id="bar-chart" width="400" height="250"></canvas>
+              
                 </div>
+
+</div>
                 
                
                
@@ -58,6 +50,7 @@
 @section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
 <script>
+
 new Chart(document.getElementById("bar-chart"), {
     type: 'bar',
     data: {
@@ -74,7 +67,6 @@ new Chart(document.getElementById("bar-chart"), {
       legend: { display: false },
       title: {
         display: true,
-        responsive: true,
         text: 'Conteo de Quejas segun Status'
       },
       scales: {
@@ -92,3 +84,4 @@ new Chart(document.getElementById("bar-chart"), {
 
 
 @endsection
+
